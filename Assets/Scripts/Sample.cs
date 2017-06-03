d23cc11a119d81e12d4b44d156ccd687b7e416bd
LAPEK\Dawid <wox0@interia.eu>
2017-06-03 05:33:17 +0200

    Added the very beginning of GameManager, Random sprites getting

---
+++ Assets/Scripts/Sample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Sample : MonoBehaviour
+{
+    private String _currentCategory;
+
+    void Start ()
+	{
+        int randomNumber = UnityEngine.Random.Range(0, GameManager.Category.Count);
+        _currentCategory = GameManager.Category.ElementAt(randomNumber);
+
+
+	    Stack<Sprite> textures = new Stack<Sprite>();
+
+	    foreach (Sprite sprite in Resources.LoadAll<Sprite>(_currentCategory))
+	    {
+            textures.Push(sprite);
+	    }
+
+
+        for (int i = 0; i < 4; i++)
+	    {
+            Shuffle(textures);
+	        Sprite texture = textures.Pop();
+            GameObject.Find("Button" + (i+1)).GetComponent<Image>().sprite = texture;      
+        }
+ 
+	}
+	
+
+    public static void Shuffle<T>(Stack<T> stack)
+    {
+        System.Random rnd = new System.Random();
+        var values = stack.ToArray();
+        stack.Clear();
+        foreach (var value in values.OrderBy(x => rnd.Next()))
+            stack.Push(value);
+    }
+
+
+}
