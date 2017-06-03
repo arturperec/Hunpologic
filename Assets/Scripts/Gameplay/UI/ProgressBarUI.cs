f12ece31c14af75f06eaf4bc3829d5ddec8d70bd
Artur Perec <artur.g.perec@gmail.com>
2017-06-03 16:39:18 +0200

    Fillbar works

---
+++ Assets/Scripts/Gameplay/UI/ProgressBarUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class ProgressBarUI : MonoBehaviour {
+    public Transform _transform;
+
+    private Vector3 _progressBarScale;
+
+
+    private void Start()
+    {
+        _progressBarScale = _transform.localScale;
+        SetFillAt(0.5f);
+    }
+
+    public void SetFillAt(float value)
+    {
+        _transform.localScale = new Vector3(value * _progressBarScale.x, _transform.localScale.y, _transform.localScale.z);
+    }
+}
---
+++ Assets/Scripts/ProgressBarFill.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarFill : MonoBehaviour
+{
+    private bool _isFinished;
+    public Image FillBarObject;
+
+    [Range(0.0f, 1.0f)]
+    public float Value;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (Value <= 1 && _isFinished == false)
+            {
+                Debug.Log("B Pressed");
+                Debug.Log("Value: " + Value);
+                Value += 0.17f;
+                SetFillAt(Value);
+            }
+            else if (Value >= 1)
+            {
+                Value = 1;
+                _isFinished = true;
+                Debug.Log("Gratulacje, wygrałeś dziwko");
+                SetFillAt(Value);
+            }
+
+        }
+    }
+
+    public void FillBar(float value)
+    {
+        if (value < 0)
+            SetFillAt(0.5f);
+        else if (value > 1)
+            SetFillAt(1);
+        else
+            SetFillAt(value);
+    }
+
+    private void SetFillAt(float value)
+    {
+        FillBarObject.fillAmount = value;
+    }
+}
