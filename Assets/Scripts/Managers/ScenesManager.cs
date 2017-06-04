feb6bfd2ea7329bf5d67dcc0ab120d735fbf58ca
Artur Perec <artur.g.perec@gmail.com>
2017-06-04 18:06:21 +0200

    You can reset the game now

--- Assets/Scripts/Managers/ScenesManager.cs
+++ Assets/Scripts/Managers/ScenesManager.cs
@@ -7,21 +7,33 @@ public class ScenesManager : MonoBehaviour {
 
     public void StartGame()
     {
+        ResetValues();
         SceneManager.LoadScene(GameManager.SettingsManager.GameSceneName);
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene(GameManager.SettingsManager.MainMenuSceneName);
+        ResetValues();
     }
 
     public void EndGameWin()
     {
         SceneManager.LoadScene(GameManager.SettingsManager.EndSceneWin);
+        ResetValues();
     }
 
     public void EndGameLose()
     {
         SceneManager.LoadScene(GameManager.SettingsManager.EndSceneLose);
+        ResetValues();
+    }
+
+    public void ResetValues()
+    {
+        Debug.Log("Values Reset");
+        GameManager.SettingsManager.FillValue = 0;
+        GameManager.SettingsManager.IsLevelFinished = false;
+        GameManager.SettingsManager.IsLevelFailed = false;
     }
 }
