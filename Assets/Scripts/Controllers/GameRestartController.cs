662fcdc08a7868c8787d56fdc78ea9889437a755
LAPEK\Dawid <wox0@interia.eu>
2017-06-04 17:11:08 +0200

    fix

--- Assets/Scripts/Controllers/GameRestartController.cs
+++ Assets/Scripts/Controllers/GameRestartController.cs
@@ -7,21 +7,35 @@ public class GameRestartController : MonoBehaviour
     private bool PlayerOneAnswerState = true;
     private bool PlayerTwoAnswerState = true;
 
+    [HideInInspector]
+    public bool isPlayerOneGood = false;
+    [HideInInspector]
+    public bool isPlayerTwoGood = false;
+
     public SpriteRandomizer SpriteRandomizer;
     public ShortcutController ShortcutController;
     public LivesController LivesController;
+    public ProgressBarFill ProgressBarFill;
+
 
 
     void Update () {
 	    if (PlayerOneAnswerState == true && PlayerTwoAnswerState == true)
 	    {
+
             for (int i = 0; i < 8; i++)
 	        {
                 ShortcutController.Buttons[i].GetComponentInChildren<ParticleSystem>().Stop();
-            }  
+            }
+	        if (isPlayerOneGood && isPlayerTwoGood)
+	        {
+                ProgressBarFill.pushOrNot = true;
+                isPlayerOneGood = false;
+                isPlayerTwoGood = false;
+            }	       
             SpriteRandomizer.Draw();
             SetPlayerOneAnswerState(false);
-	        SetPlayerTwoAnswerState(false);
+	        SetPlayerTwoAnswerState(false);     
 	    }
 
         if (GameManager.SettingsManager.IsLevelFailed)
